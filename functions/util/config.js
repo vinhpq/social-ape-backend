240c401e3987e4ab4488a2a5c803c8f4811bdfbd
linux <vinhfq@gmail.com>
2020-11-13 18:14:58 +0700

    First commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBfDGY3YIEU9smFQTclhwvFBjAoa8fuCbU",
+    authDomain: "socialape-83ba8.firebaseapp.com",
+    databaseURL: "https://socialape-83ba8.firebaseio.com",
+    projectId: "socialape-83ba8",
+    storageBucket: "socialape-83ba8.appspot.com",
+    messagingSenderId: "486148119624",
+    appId: "1:486148119624:web:e28fc05c9ca3d7337bcc7d",
+    measurementId: "G-S43Z46VHXT"
+  };
